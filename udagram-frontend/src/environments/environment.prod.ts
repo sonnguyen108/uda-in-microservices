bde9aad1f74ade02588b5455ff0dc9bbeda5bb90
sonson <sonson@hotmail.com>
2024-12-02 17:57:15 +0000

    fix node version

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a731eac1325e14c11a89c0e016904838-640515363.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
